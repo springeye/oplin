d7fbef59a3c967e5b370ac3e73d4510d321e9683
springeye <henjue@gmail.com>
2022-06-25 13:09:26 +0800

    add build_dmg.sh

---
+++ build_dmg.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if  command -v create-dmg > /dev/null; then
+else
+    echo "create-dmg command has not found"
+    echo "please install https://github.com/create-dmg/create-dmg"
+    exit 1
+fi
+
+
+rm -rf Release
+rm *.dmg
+mkdir Release
+cp -r build/macos/Build/Products/Release/oplin.app Release/Oplin.app
+create-dmg \
+  --volname "Oplin Installer" \
+  --window-pos 200 120 \
+  --window-size 800 400 \
+  --icon-size 100 \
+  --icon "Oplin.app" 200 190 \
+  --hide-extension "Oplin.app" \
+  --app-drop-link 600 185 \
+  "Oplin-Installer.dmg" \
+  "Release/"
